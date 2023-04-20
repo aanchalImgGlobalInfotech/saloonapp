7507a5a91890efadb0040fbb8d5b3bf68be73c13
prashant-img <prashanttiwari.img@gmail.com>
2023-04-20 09:53:08 +0530

    verify otp

--- src/pages/login/verifyotp.js
+++ src/pages/login/verifyotp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import OtpInput from "react-otp-input";
 import { useDispatch } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
@@ -6,16 +6,27 @@ import { postData } from "../../components/apiinstance/Api";
 import { setUsers } from "../../components/redux/redux1/actions";
 import Footer from "../../common/layout/footer/footer";
 import { ToastContainer, toast } from "react-toastify";
+import { number } from "yup";
 
 function Verifyotp() {
   let location = useLocation();
+  const ref1 = useRef(null);
+  const ref2 = useRef(null);
+  const ref3 = useRef(null);
+  const ref4 = useRef(null);
   const dispatch = useDispatch();
   console.log(location.state);
   const navigate = useNavigate();
-  const [otp, setOtp] = useState('');
+  const [otp, setOtp] = useState({
+    otp1: "",
+    otp2: "",
+    otp3: "",
+    otp4: "",
+  });
   const [minutes, setMinutes] = useState(1);
   const [seconds, setSeconds] = useState(1);
- const phoneNum = localStorage.getItem('phone')
+  const [otpvalue, setOtpValue] = useState("");
+  const phoneNum = localStorage.getItem("phone");
   useEffect(() => {
     const interval = setInterval(() => {
       if (seconds > 0) {
@@ -37,7 +48,7 @@ function Verifyotp() {
     };
   }, [seconds]);
 
-  const resendOTP = async(e) => {
+  const resendOTP = async (e) => {
     var body = {
       phone: phoneNum,
     };
@@ -45,47 +56,78 @@ function Verifyotp() {
     setMinutes(1);
     setSeconds(1);
   };
+
+  var val = otp.otp1 + otp.otp2 + otp.otp3 + otp.otp4;
+  console.log(typeof val);
+  useEffect(() => {
+    let data = Number(val);
+    console.log(data);
+    if (otp.otp1 != "" && otp.otp2 != "" && otp.otp3 != "" && otp.otp4 != "") {
+      genOtp(data);
+    }
+  }, [otp.otp1, otp.otp2, otp.otp3, otp.otp4]);
   const handleChange = (e) => {
     let a = e.target.value;
-    console.log('ototototppp', a)
-    console.log(a);
-    setOtp((prev) => prev + a);
-    if (e.target.name == "otp4") {
-      genOtp(e.target.value);
+    let n = e.target.name;
+    setOtp({
+      ...otp,
+      [e.target.name]: a,
+    });
+    if (n == "otp1" && a) {
+      ref2.current.focus();
+    } else if (n == "otp2" && a) {
+      ref3.current.focus();
+    } else if (n == "otp3" && a) {
+      ref4.current.focus();
+    } else {
+      if (n == "otp4" && !a) {
+        ref3.current.focus();
+      } else if (n == "otp3" && !a) {
+        ref2.current.focus();
+      } else if (n == "otp2" && !a) {
+        ref1.current.focus();
+      }
     }
   };
-
-  const genOtp = async (digit) => {
+  useEffect(() => {
+    ref1.current.focus();
+  }, []);
+  const genOtp = async (val) => {
     var body = {
-      otp: Number(otp + digit),
+      otp: val,
       phone: phoneNum,
     };
-    const res = await postData("login-otp-verify", body);
-    if(res.status){
-      toast.success(res.message,{
-        position: toast.POSITION.TOP_RIGHT,})
-        dispatch(setUsers(res.data));
-        setTimeout(() => {
-          navigate("/Dashboard");
-        }, 2000);
-    }else{
-       toast.error(res.message,{
-        position:toast.POSITION.TOP_RIGHT
-       })
-    }
-    let token = localStorage.setItem("token", res?.data[1]?.auth);
-    console.log(res, "ressssss56789");
-    setMinutes(0);
-    setSeconds(0);
+    await postData("login-otp-verify", body)
+      .then((response) => {
+        if (response.status) {
+          toast.success(response.message, {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+          dispatch(setUsers(response.data));
+          setTimeout(() => {
+            navigate("/Dashboard");
+          }, 2000);
+        } else {
+          toast.error(response.message, {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        }
+        let token = localStorage.setItem("token", response?.data[1]?.auth);
+        console.log(response, "ressssss56789");
+        setMinutes(0);
+        setSeconds(0);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
   };
-
-
-
+  const handleKeyDown = (e) => {
+    console.log("handleKeyDown jjdh", e);
+  };
   return (
     <div>
       <div className="container-fluid login px-lg-5">
-      <ToastContainer
-        autoClose ='2000'/>
+        <ToastContainer autoClose="2000" />
         <div className="login2 h-100">
           <div className="row h-100 align-items-center">
             <div className="col-md-5 col-12  order-md-1 order-2 h-100 d-flex flex-column justify-content-center mt-5 px-sm-5">
@@ -111,8 +153,10 @@ function Verifyotp() {
                       <input
                         className="m-2 text-center form-control rounded formotp"
                         // value={otp}
+                        ref={ref1}
                         name="otp1"
                         onChange={handleChange}
+                        onKeyPress={handleKeyDown}
                         type="text"
                         id="first"
                         maxLength={1}
@@ -120,8 +164,10 @@ function Verifyotp() {
                       <input
                         className="m-2 text-center form-control rounded formotp"
                         // value={otp}
+                        ref={ref2}
                         name="otp2"
                         onChange={handleChange}
+                        onKeyPress={handleKeyDown}
                         type="text"
                         id="second"
                         maxLength={1}
@@ -129,8 +175,10 @@ function Verifyotp() {
                       <input
                         className="m-2 text-center form-control rounded formotp"
                         // value={otp}
-                        name="opt3"
+                        ref={ref3}
+                        name="otp3"
                         onChange={handleChange}
+                        onKeyPress={handleKeyDown}
                         type="text"
                         id="third"
                         maxLength={1}
@@ -138,8 +186,10 @@ function Verifyotp() {
                       <input
                         className="m-2 text-center form-control rounded formotp"
                         // value={otp}
+                        ref={ref4}
                         name="otp4"
                         onChange={handleChange}
+                        onKeyPress={handleKeyDown}
                         type="text"
                         id="fourth"
                         maxLength={1}
@@ -164,7 +214,10 @@ function Verifyotp() {
                                   ? "#DFE3E8"
                                   : "#FF5630",
                             }}
-                            onClick={() => {resendOTP(); setOtp('');}}
+                            onClick={() => {
+                              resendOTP();
+                              setOtp("");
+                            }}
                           >
                             Resend OTP
                           </button>
@@ -175,8 +228,7 @@ function Verifyotp() {
                     <div className="or-border position-relative text-theme1">
                       <span>or</span>
                     </div>
-                    <div className="my-3 text-center">
-                    </div>
+                    <div className="my-3 text-center"></div>
                   </div>
                 </form>
                 <div className="text-center pb-3">
